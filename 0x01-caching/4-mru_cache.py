75677cde08579f97f163aa16c0568ad400ebf8a3
abeermosaad <abeermosaad69@gmail.com>
2024-02-05 10:24:06 +0200

    [Add] Task 4 again

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Basic dictionary """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache (BaseCaching):
+    """ FIFOCache class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """ Constructor of the class"""
+        super().__init__()
+
+    def move_to_end(self, key):
+        """move_to_end func"""
+        if key in self.cache_data:
+            value = self.cache_data.pop(key)
+            self.cache_data[key] = value
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+
+            self.cache_data[key] = item
+            self.move_to_end(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print(f'DISCARD: {list(self.cache_data.keys())[-2]}')
+                del self.cache_data[list(self.cache_data.keys())[-2]]
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if not key or key not in self.cache_data:
+            return None
+        self.move_to_end(key)
+        return self.cache_data[key]
