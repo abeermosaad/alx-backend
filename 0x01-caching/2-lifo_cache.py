0552c43d6a32ee026311426ef644f02001f26edb
abeermosaad <abeermosaad69@gmail.com>
2024-02-05 09:15:31 +0200

    [Add] Task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Basic dictionary """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache (BaseCaching):
+    """ FIFOCache class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """ Constructor of the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if key in self.cache_data:
+                del self.cache_data[key]
+
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print(f'DISCARD: {list(self.cache_data.keys())[-2]}')
+                del self.cache_data[list(self.cache_data.keys())[-2]]
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
